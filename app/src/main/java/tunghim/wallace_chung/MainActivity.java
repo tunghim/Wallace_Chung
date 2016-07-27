b05edd0abf5d1f44bb24909d21a6733268eac931
tunghim <tunghim@gmail.com>
2016-07-27 09:23:19 +0800

    Solved image problem, changed to real pics.

--- app/src/main/java/tunghim/wallace_chung/MainActivity.java
+++ app/src/main/java/tunghim/wallace_chung/MainActivity.java
@@ -6,41 +6,15 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
-
-    String s = "Next";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button next = (Button) findViewById(R.id.next);
-        next.setText(s);
-
-        next.setOnClickListener(new View.OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                if (s.equals("Next")) {
-                    ImageView img = (ImageView) findViewById(R.id.imageView);
-
-                    img.setImageResource(R.drawable.salmoon);
-                    Button next = (Button) findViewById(R.id.next);
-                    s = "Prev";
-                    next.setText(s);
-                } else {
-                    ImageView img = (ImageView) findViewById(R.id.imageView);
-                    img.setImageResource(R.drawable.lobster);
-                    Button next = (Button) findViewById(R.id.next);
-                    s = "Next";
-                    next.setText(s);
-                }
-            }
-        });
     }
-
-
 }
