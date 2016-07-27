52dcc4b409f9c269c2aa4f76845ce10d6ea8b614
tunghim <tunghim@gmail.com>
2016-07-27 12:44:56 +0800

    Done!

--- app/src/main/java/tunghim/wallace_chung/MainActivity.java
+++ app/src/main/java/tunghim/wallace_chung/MainActivity.java
@@ -1,14 +1,15 @@
 package tunghim.wallace_chung;
 
-import android.provider.ContactsContract;
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
+import android.text.Spanned;
+import android.text.method.LinkMovementMethod;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.Button;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView name = (TextView) findViewById(R.id.name);
+        Spanned text = Html.fromHtml("By <a href='https://twitter.com/tunghim'>@tunghim</a>");
+        name.setMovementMethod(LinkMovementMethod.getInstance());
+        name.setText(text);
+
+        final ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.w_audio);
+
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+
+                    if (event.getAction() == MotionEvent.ACTION_DOWN) {
+                        mp.start();
+                        imageView.setImageResource(R.drawable.w_10_2);
+                        return true;
+                    } else if (event.getAction() == MotionEvent.ACTION_UP) {
+                        imageView.setImageResource(R.drawable.w_4_2);
+                        mp.pause();
+                        return true;
+                    }
+
+                return false;
+            }
+        });
     }
 }
